fbf3183778d19121b6e13add3b3ca2c857d7f0ee
John Eblen <jeblen@acm.org>
2016-06-24 14:27:48 -0700

    Fix GCC compile error when initializing atomic
    sts/sts.cpp:4:38: error: use of deleted function
    'std::atomic<int>::atomic(const std::atomic<int>&)'
     std::atomic<int> STS::stepCounter_ = 0;

--- sts/sts.cpp
+++ sts/sts.cpp
@@ -1,7 +1,7 @@
 #include "sts.h"
 
 std::deque<Thread> STS::threads_ = {};
-std::atomic<int> STS::stepCounter_ = 0;
+std::atomic<int> STS::stepCounter_(0);
 STS* STS::defaultInstance_ = nullptr;
 std::map<std::string, STS *> STS::stsInstances_ = {};
 STS* STS::instance_ = nullptr;
